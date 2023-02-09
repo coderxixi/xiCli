0b573148f616b75af8dc824ca0e5b96fc0832a3e
liuss7 <976344695@qq.com>
2023-02-09 16:19:12 +0800

    uilt 工具库的开发

--- bin/root/index.js
+++ bin/root/index.js
@@ -10,26 +10,26 @@ function init(cmdPath, name, option) {
         path.resolve(cmdPath, name)
     );
 
-    // init readme
-    copyTmpl(
-        path.resolve(__dirname, `./template/README.md.tmpl`),
-        path.resolve(cmdPath, name, "README.md"),
-        option
-    );
+    // // init readme
+    // copyTmpl(
+    //     path.resolve(__dirname, `./template/README.md.tmpl`),
+    //     path.resolve(cmdPath, name, "README.md"),
+    //     option
+    // );
 
-    // init license
-    copyTmpl(
-        path.resolve(__dirname, `./template/license.tmpl`),
-        path.resolve(cmdPath, name, "LICENSE"),
-        option
-    );
+    // // init license
+    // copyTmpl(
+    //     path.resolve(__dirname, `./template/license.tmpl`),
+    //     path.resolve(cmdPath, name, "LICENSE"),
+    //     option
+    // );
 
-    // init package
-    copyTmpl(
-        path.resolve(__dirname, `./template/package.json.tmpl`),
-        path.resolve(cmdPath, name, 'package.json'),
-        option,
-    );
+    // // init package
+    // copyTmpl(
+    //     path.resolve(__dirname, `./template/package.json.tmpl`),
+    //     path.resolve(cmdPath, name, 'package.json'),
+    //     option,
+    // );
 }
 
 module.exports.init = init;
